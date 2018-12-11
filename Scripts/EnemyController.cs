7327019671106401921b7682121ae36f656ca02b
R. Comonfort <a.comonfort@outlook.com>
2018-12-11 16:44:13 -0600

    Began working on enemies

---
+++ Scripts/EnemyController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+
+public class EnemyController : MonoBehaviour {
+
+	public float detectionRadius = 5f;
+
+	Transform target;
+	Enemy pawn;
+	NavMeshAgent agent;
+
+	// Use this for initialization
+	void Start () {
+		target = PlayerManager.instance.player.transform;
+		pawn = GetComponent<Enemy>();
+		agent = GetComponent<NavMeshAgent>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	private void OnDrawGizmosSelected()
+	{
+		Gizmos.color = Color.red;
+		Gizmos.DrawWireSphere(transform.position, detectionRadius);
+	}
+}
